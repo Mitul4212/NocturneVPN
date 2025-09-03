376748dadf9114ca9ea0a72d8f495f01d0fde9b2
Mitul <mitul4212a@gmail.com>
2025-09-04 00:35:53 +0530

    change package name for remove .example, add proguard rules for not removing any classes which is needed, update apl level 33 to 35

--- app/src/main/java/com/nocturnevpn/view/activitys/SplashActivity.kt
+++ app/src/main/java/com/nocturnevpn/view/activitys/SplashActivity.kt
@@ -1,4 +1,4 @@
-package com.example.nocturnevpn.view.activitys
+package com.nocturnevpn.view.activitys
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,9 +6,9 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.nocturnevpn.R
-import com.example.nocturnevpn.utils.AuthManager
-import com.example.nocturnevpn.utils.AuthFlowManager
+import com.nocturnevpn.R
+import com.nocturnevpn.utils.AuthManager
+import com.nocturnevpn.utils.AuthFlowManager
 import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
