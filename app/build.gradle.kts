64ff585961c7a67b3280bf0737dd1f9978f342cf
Mitul <mitul4212a@gmail.com>
2025-09-26 17:21:21 +0530

    add billing dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.nocturnevpn"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.3"
+        versionCode = 6
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -140,5 +140,7 @@ dependencies {
 
     // Google Ads SDK for banner ads
     implementation("com.google.android.gms:play-services-ads:22.6.0")
+
+    implementation("com.android.billingclient:billing-ktx:7.0.0")
 
 }
